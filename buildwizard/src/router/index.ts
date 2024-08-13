5ef72497d1cbb866b0c72bbe56d5fe38087ec4c6
zsolti <82903515+zslti@users.noreply.github.com>
2024-08-13 13:54:23 +0300

    BW-3 As a user, I want a consistent header layout with a menu button across the application so that I can easily access navigation options from any page

--- buildwizard/src/router/index.ts
+++ buildwizard/src/router/index.ts
@@ -7,12 +7,11 @@ const routes: Array<RouteRecordRaw> = [
     name: "home",
     component: HomeView,
   },
-  // {
-  //   path: "/about",
-  //   name: "about",
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  // },
+  {
+    path: "/builds",
+    name: "builds",
+    component: HomeView,
+  },
 ];
 
 const router = createRouter({
