52b1ec5dec388aa3635da3c76c326d1696f353ee
LaszloZsolt2 <laszlo.zsolt@student.ms.sapientia.ro>
2024-08-13 17:09:25 +0300

    BW-4 As a user, I want a consistent footer layout across the application so that the application has a professional and cohesive look on every page

--- buildwizard/src/App.vue
+++ buildwizard/src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <!-- TODO: put MainHeader in MainLayout.vue -->
-  <MainHeader />
-  <router-view />
+  <div class="app flex flex-col min-h-screen">
+    <div class="flex-1">
+      <MainHeader />
+
+      <router-view />
+    </div>
+    <MainFooter />
+  </div>
 </template>
 
 <script setup lang="ts">
 import MainHeader from "./components/MainHeader.vue";
+import MainFooter from "./components/MainFooter.vue";
 import "./assets/output.css";
 </script>
 
 <style>
 body {
   @apply bg-neutral-900;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.content {
+  flex: 1;
 }
 </style>
---
+++ buildwizard/src/components/MainFooter.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer class="bg-violet-800 text-white p-4">
+    <div
+      class="container mx-auto flex flex-col md:flex-row justify-between items-center"
+    >
+      <div class="mb-4 md:mb-0">
+        <p>&copy; 2024 BuildWizard. Your guide to compatible PC components.</p>
+      </div>
+      <nav class="flex space-x-4">
+        <SvgIcon
+          name="facebook"
+          href="https://www.facebook.com"
+          class="w-6 h-6 text-white"
+        />
+        <SvgIcon
+          name="email"
+          href="https://www.gmail.com"
+          class="w-6 h-6 text-white"
+        />
+        <SvgIcon
+          name="linkedin"
+          href="https://www.linkedin.com"
+          class="w-6 h-6 text-white"
+        />
+      </nav>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import SvgIcon from "@/components/SvgIcon.vue";
+
+export default defineComponent({
+  name: "MainFooter",
+  components: {
+    SvgIcon,
+  },
+});
+</script>
