76a1dfb5dcc337910ef14b547123d7161e829038
zsolti <82903515+zslti@users.noreply.github.com>
2024-08-21 13:25:28 +0300

    BW-9 Added cors configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import caseFanRouter from "./routes/CaseFans";
 import caseRouter from "./routes/Cases";
@@ -20,6 +21,7 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/default";
 
 app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect(MONGO_URI)
