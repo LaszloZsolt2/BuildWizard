75d34799753a4665f21bb3d099ca82e63aaeacab
zsolti <82903515+zslti@users.noreply.github.com>
2024-09-03 13:57:33 +0300

    BW-24 Removed unnecessary async keyword

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,7 @@ mongoose
   });
 const db = mongoose.connection;
 
-db.on("connected", async () => {
+db.on("connected", () => {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
   console.log(`Database name: ${db.name}`);
 
